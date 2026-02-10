3d47fc7a37e739c1d89f599225926fc413c50c68
Chaesar Kusuma <chaesarputra14@gmail.com>
2026-02-10 11:22:45 +0700

    last

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        account: 'account.html',
+        checkout: 'checkout.html',
+        index: 'index.html',
+        login: 'login.html',
+        register: 'register.html',
+        wishlist: 'wishlist.html',
+        shop: 'shop.html',
+        view: 'view.html',
+
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
